da6250a40efae00ec5329ec714520e8b4ef52dc7
Michael Eco <michael.gunawan2002@gmail.com>
2025-12-27 14:18:34 +0700

    feat: added create_article_description feature engineering

--- core/features/articles.py
+++ core/features/articles.py
@@ -6,3 +6,14 @@ def get_article_id(df: pl.DataFrame) -> pl.Series:
 def create_prod_name_length(df: pl.DataFrame) -> pl.Series:
     return df["prod_name"].str.len_chars()
 
+def create_article_description(row):
+    description = f"{row['prod_name']} - {row['product_type_name']} in {row['product_group_name']}"
+    description += f"\nAppearance: {row['graphical_appearance_name']}"
+    description += f"\nColor: {row['perceived_colour_value_name']} {row['perceived_colour_master_name']} ({row['colour_group_name']})"
+    description += f"\nCategory: {row['index_group_name']} - {row['section_name']} - {row['garment_group_name']}"
+
+    if row['detail_desc']:
+        description += f"\nDetails: {row['detail_desc']}"
+
+    return description
+
