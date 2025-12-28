7a59624f5d9c54454ffdeb882c7c3c6d6c26139e
Michael Eco <michael.gunawan2002@gmail.com>
2025-12-28 13:28:13 +0700

    feat: added seasonal feature

--- core/features/transactions.py
+++ core/features/transactions.py
@@ -1,3 +1,4 @@
+import numpy as np
 import polars as pl
 import pandas as pd
 
@@ -18,4 +19,16 @@ def get_day_feature(df: pl.DataFrame) -> pl.Series:
 
 def get_day_of_week_feature(df: pl.DataFrame) -> pl.Series:
     return df["t_dat"].dt.weekday()
+
+def calculate_month_sin_cos(month: pl.Series) -> pl.DataFrame:
+    C = 2 * np.pi / 12
+    return pl.DataFrame(
+        {
+            "month_sin": month.apply(lambda x: np.sin(x * C)),
+            "month_cos": month.apply(lambda x: np.cose(x * C))
+        }
+    )
+
+def convert_t_dat_to_epoch_milliseconds(df: pl.DataFrame) -> pl.Series:
+    return df["t_dat"].cast(pl.Int64)
 
