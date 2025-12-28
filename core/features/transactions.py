a46279b0c5f387a335a019b7a60671d149a7bc52
Michael Eco <michael.gunawan2002@gmail.com>
2025-12-28 13:26:18 +0700

    feat: added function for get month, day, and day of week feature

--- core/features/transactions.py
+++ core/features/transactions.py
@@ -9,3 +9,13 @@ def convert_t_dat_to_datetime(df: pl.DataFrame) -> pl.Series:
 
 def get_year_feature(df: pl.DataFrame) -> pl.Series:
     return df["t_dat"].dt.year()
+
+def get_month_feature(df: pl.DataFrame) -> pl.Series:
+    return df["t_dat"].dt.month()
+
+def get_day_feature(df: pl.DataFrame) -> pl.Series:
+    return df["t_dat"].dt.day()
+
+def get_day_of_week_feature(df: pl.DataFrame) -> pl.Series:
+    return df["t_dat"].dt.weekday()
+
