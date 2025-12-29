2476717ea2d6084b1882eed5ad7e5e833e18ee52
Michael Eco <michael.gunawan2002@gmail.com>
2025-12-29 11:58:58 +0700

    feat: added compute_features_transactions function

--- core/features/transactions.py
+++ core/features/transactions.py
@@ -41,3 +41,24 @@ def month_sin(month: pd.Series):
 def month_cos(month: pd.Series):
     return np.cose(month * (2 * np.pi / 12))
 
+def compute_features_transactions(df: pl.DataFrame) -> pl.DataFrame:
+    return (
+        df.with_columns(
+            [
+                pl.col("article_id").cast(pl.Utf8).alias("article_id")
+            ]
+        )
+        .with_columns(
+            [
+                pl.col("t_dat").dt.year().alias("year"),
+                pl.col("t_dat").dt.month().alias("month"),
+                pl.col("t_dat").dt.day().alias("day"),
+                pl.col("t_dat").dt.weekday().alias("day_of_week")
+            ]
+        )
+        .with_columns(
+            [
+                (pl.col("t_dat").cast(pl.Int64) // 1_000_000).alias("t_dat")
+            ]
+        )
+    )
