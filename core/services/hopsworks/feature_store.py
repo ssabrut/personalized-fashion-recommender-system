fbfae2809e02e96736b9f57f58da357feead21c1
Michael Eco <michael.gunawan2002@gmail.com>
2025-12-29 12:43:19 +0700

    feat: added get feature store function

--- core/services/hopsworks/feature_store.py
+++ core/services/hopsworks/feature_store.py
@@ -1,1 +1,14 @@
-from core.config import get_settings
+import hopsworks
+
+from core.config import settings
+
+def get_feature_store():
+    if settings.HOPSWORKS_API_KEY:
+        project = hopsworks.login(
+            api_key_value=settings.HOPSWORKS_API_KEY.get_secret_value()
+        )
+    else:
+        project = hopsworks.login()
+
+    return project, project.get_feature_store()
+
