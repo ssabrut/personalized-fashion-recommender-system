0933cad5e6f2e5dc82234c8c4e2c22f9f594e888
Michael Eco <michael.gunawan2002@gmail.com>
2025-12-29 12:45:22 +0700

    feat: added customer feature group creation function

--- core/services/hopsworks/feature_store.py
+++ core/services/hopsworks/feature_store.py
@@ -1,6 +1,8 @@
 import hopsworks
+import pandas as pd
 
 from core.config import settings
+from core import constants
 
 def get_feature_store():
     if settings.HOPSWORKS_API_KEY:
@@ -12,3 +14,18 @@ def get_feature_store():
 
     return project, project.get_feature_store()
 
+def create_customers_feature_group(fs, df: pd.DataFrame, online_enabled: bool = True):
+    customers_fg = fs.get_or_create_feature_group(
+        name="customers",
+        description="Customers data including age and postal code",
+        version=1,
+        primary_key=["customer_id"],
+        online_enabled=online_enabled
+    )
+
+    customers_fg.insert(df, wait=True)
+
+    for desc in constants.CUSTOMER_FEATURE_DESCRIPTIONS:
+        customers_fg.update_feature_description(desc["name"], desc["description"])
+
+    return customers_fg
